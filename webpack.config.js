a02311bbd2f1ad3b061c5820922bc32af9da6f30
Guy Balaam <guy.balaam@gmail.com>
2021-04-24 14:43:06 +0100

    Adding devServer

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const { resolve } = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const BUILD_DIR = resolve(__dirname, 'dist');
+
 module.exports = {
     mode: 'production',
     entry: './src/index.ts',
@@ -14,11 +16,17 @@ module.exports = {
         ],
     },
     resolve: {
-        extensions: ['.ts'],
+        extensions: ['.ts', '.js'],
     },
     output: {
         filename: 'bundle.js',
-        path: resolve(__dirname, 'dist'),
+        path: BUILD_DIR
     },
-    plugins: [new HtmlWebpackPlugin()]
+    devServer: {
+        contentBase: BUILD_DIR,
+        port: 3000
+    },
+    plugins: [new HtmlWebpackPlugin({
+        title: 'Tower Defense'
+    })]
 };
