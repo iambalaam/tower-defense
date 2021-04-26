638ac2b476a7d6a5b648df1fc6dfd94ba92f6b91
Guy Balaam <guy.balaam@gmail.com>
2021-04-26 14:43:49 +0100

    Separating asset loading logic

--- src/App.ts
+++ src/App.ts
@@ -9,30 +9,48 @@ const isDebug = window.location.search
     .includes('debug');
 
 export class App {
+    _loading = true;
+    _imageBitmaps?: ImageBitmap[];
     _canvas: CanvasRenderer;
-    _img?: HTMLImageElement;
     _prevMs: number;
     _grid?: SpriteGrid;
 
     constructor(canvas: CanvasRenderer) {
+
         this._canvas = canvas;
         this._prevMs = 0;
 
-        const img = new Image();
-        img.src = './Ground.png';
-        img.onload = () => {
-            createImageBitmap(img)
-                .then((bitmap) => {
-                    const s = new Sprite(bitmap);
-                    this._grid = new SpriteGrid(this._canvas, [
-                        [[s, s, s, s, s], [s, s, s, s], [s, s, s], [s, s], [s]],
-                        [[s, s, s], [s, s], [s]],
-                        [[s]]
-                    ]);
-                })
-        }
+        this.loadAssets()
+            .then(() => {
+                const s = new Sprite(this._imageBitmaps![0]);
+                this._grid = new SpriteGrid(this._canvas, [
+                    [[s, s, s, s, s], [s, s, s, s], [s, s, s], [s, s], [s]],
+                    [[s, s, s], [s, s], [s]],
+                    [[s]]
+                ]);
+            });
+
+
 
         this.renderLoop(0);
+    }
+
+    async loadAssets() {
+        const imageURLs = ['./Ground.png'];
+
+        const imageElementPromises = imageURLs.map((url) => new Promise<HTMLImageElement>((res, _rej) => {
+            const img = new Image();
+            img.src = url;
+            img.onload = () => {
+                res(img);
+            }
+        }));
+
+        const imageElements = await Promise.all(imageElementPromises);
+
+        const imageBitmapPromises = imageElements.map((img) => createImageBitmap(img));
+        const imageBitmaps = await Promise.all(imageBitmapPromises);
+        this._imageBitmaps = imageBitmaps;
     }
 
     renderSprites() {
